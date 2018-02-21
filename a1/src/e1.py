3967290136e74b628b720e6abbb67546d3a6af15
OliverScherf <scherf.oliver@gmx.net>
2018-02-21 14:38:10 +0100

    [REFAC] Assignment1

--- a1/src/e1.py
+++ a1/src/e1.py
@@ -1,34 +1,47 @@
+'''
+Created on 10.02.2018
+
+@author: Matthias Müller-Brockhausen & Oliver Scherf
+'''
 import numpy as np
 
-trainIn = np.genfromtxt('../data/train_in.csv', delimiter=',')
-trainOut = np.genfromtxt('../data/train_out.csv', delimiter=',')
+def euclidianDistance(a, b):
+    return np.absolute(np.linalg.norm(a - b))
 
-i = 0
-meanSum = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-meanOccurence = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-cloud = [[],[],[],[],[],[],[],[],[],[]]
+def main():
+    trainIn = np.genfromtxt('../data/train_in.csv', delimiter=',')
+    trainOut = np.genfromtxt('../data/train_out.csv', delimiter=',')
+    
+    meanSum = [0] * 10
+    meanOccurence =  [0] * 10
+    cloud = [ [] for i in range(10) ]
+    
+    i = 0
+    for _ in np.nditer(trainOut.T):
+      recognizedNumber = int(trainOut[i])
+      meanSum[recognizedNumber] += trainIn[i]
+      meanOccurence[recognizedNumber] += 1
+      cloud[recognizedNumber].append(trainIn[i])
+      i += 1
+    
+    center = [0] * 10
+    radius = [0] * 10
+    for b in range(0, 10):
+      center[b] = meanSum[b] / meanOccurence[b]
+      for img in cloud[b]:
+        res = euclidianDistance(center[b], img)
+        if res > radius[b]:
+          radius[b] = res
+    
+    
+    totalDistance = np.zeros([10, 10])
+    for b in range(0, 10):
+      for c in range(0, 10):
+        totalDistance[b][c] += euclidianDistance(center[b], center[c])
+    
+    print("distances are")
+    print(totalDistance)
+    np.savetxt("distances.csv", totalDistance, delimiter=",")
 
-for imgToBeRecognized in np.nditer(trainOut.T):
-  recognizedNumber = int(trainOut[i])
-  meanSum[recognizedNumber] += trainIn[i]
-  meanOccurence[recognizedNumber] += 1
-  cloud[recognizedNumber].append(trainIn[i])
-  i += 1
-
-center = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-radius = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-for b in range(0, 10):
-  center[b] = meanSum[b] / meanOccurence[b]
-  for img in cloud[b]:
-    res = np.absolute(np.linalg.norm(center[b] - img))
-    if res > radius[b]:
-      radius[b] = res
-
-
-totalDistance = np.zeros([10, 10])
-for b in range(0, 10):
-  for c in range(0, 10):
-    totalDistance[b][c] += np.absolute(np.linalg.norm(center[b] - center[c]))
-
-print("distances are ", totalDistance)
-np.savetxt("distances.csv", totalDistance, delimiter=",")
+if __name__ == "__main__":
+    main()
